7fce704f0ab8a2c7c08f832eb657418a8afc13b5
Neshaki091 <huynhluyen730@gmail.com>
2025-03-20 09:05:09 +0700

    homepage

---
+++ project/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:project/Page/home_screen.dart';
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.deepPurple),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
